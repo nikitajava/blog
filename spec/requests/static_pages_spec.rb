d9d8db26a2d846a99e37e61e5b28cc32674a4c9d
nikitajava <nikita061095@yandex.ru>
2016-06-05 22:30:20 +0300

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Blog'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Blog')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+     it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Blog | About Us")
+    end
+  end
+end
